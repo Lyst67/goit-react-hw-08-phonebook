224588c7306d166bee7e182b7a1d11203b286834
Lyst67 <p_listopad@ukr.net>
2024-02-02 17:57:03 +0200

    old

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import { useSelector } from 'react-redux';
-import css from './Home.module.css'
+import css from './Home.module.css';
 import { selectUserName } from 'redux/auth/auth-selectors';
+
 const HomePage = () => {
-    const userName = useSelector(selectUserName)
-    return (
-        <div className={css.home_page}>
-            <h1 className={css.title}>Hello {userName}!<br/>Welcome to the Phonebook service!</h1>
-        </div>
-    )
-}
+  const userName = useSelector(selectUserName);
+  return (
+    <div className={css.home_page}>
+      <h1 className={css.title}>
+        Hello {userName}!<br />
+        Welcome to the Phonebook service!
+      </h1>
+    </div>
+  );
+};
 export default HomePage;
